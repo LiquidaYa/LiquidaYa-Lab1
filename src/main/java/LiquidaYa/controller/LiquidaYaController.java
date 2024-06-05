66d2a00cbbeba4df7c9a679bcce4ba7c89fb5868
SangreNueva3d <angel.yepes2@udea.edu.co>
2024-06-05 02:30:42 -0500

    tercer

--- src/main/java/LiquidaYa/controller/LiquidaYaController.java
+++ src/main/java/LiquidaYa/controller/LiquidaYaController.java
@@ -1,15 +1,13 @@
 package LiquidaYa.controller;
 
-import java.util.Collections;
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import LiquidaYa.exception.ListVehicleException;
 import LiquidaYa.exception.PlateException;
@@ -18,7 +16,11 @@ import LiquidaYa.repository.impl.VehicleRepositoryImpl;
 import LiquidaYa.service.LiquidaYaService;
 import LiquidaYa.service.impl.LiquidaYaServiceImpl;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500") // Permite el acceso desde tu frontend
 public class LiquidaYaController {
 
     private final LiquidaYaService liquidaYaService;
@@ -28,18 +30,14 @@ public class LiquidaYaController {
     }
 
     @GetMapping("/vehiculos")
-    public ResponseEntity<List<Vehicle>> getAll() throws ListVehicleException{
-        try{
-            return ResponseEntity.status(HttpStatus.OK ).body(liquidaYaService.getAll());
-
-        }catch(ListVehicleException e){
-
+    public ResponseEntity<List<Vehicle>> getAll() throws ListVehicleException {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(liquidaYaService.getAll());
+        } catch (ListVehicleException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .header("ERROR NAME", e.getMessage())
-            .body(Collections.emptyList());
+                    .header("ERROR NAME", e.getMessage())
+                    .body(Collections.emptyList());
         }
-
-        
     }
 
     @GetMapping("/vehiculos/{plate}")
@@ -58,8 +56,7 @@ public class LiquidaYaController {
     }
 
     @PostMapping("/vehiculos")
-    public ResponseEntity<Vehicle> add(@RequestBody Vehicle vehicle){
+    public ResponseEntity<Vehicle> add(@RequestBody Vehicle vehicle) {
         return ResponseEntity.status(HttpStatus.OK).body(liquidaYaService.add(vehicle));
     }
-
 }
